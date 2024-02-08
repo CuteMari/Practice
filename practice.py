273994b4edc88da95fd87e7503768fd1518f5840
yumin <pikahchoo200@gmail.com>
2024-02-08 23:03:01 +0900

    [백준]1916

--- practice.py
+++ practice.py
@@ -2,35 +2,33 @@ import sys
 input = sys.stdin.readline
 from queue import PriorityQueue
 
-V, E = map(int, input().split())
-K = int(input())
-myList = [[] for _ in range(V+1)]
-distance = [sys.maxsize]*(V+1)
-visited = [False]*(V+1)
+N = int(input())
+M = int(input())
+bus = [[] for _ in range(N+1)]
+D = [sys.maxsize]*(N+1)
+visited = [False]*(N+1)
 q = PriorityQueue()
 
-for _ in range(E):
+for _ in range(M):
     u, v, w = map(int, input().split())
-    myList[u].append((v,w))
+    bus[u].append((v,w))
 
-q.put((0,K))
-distance[K] = 0
+startDosi, endDosi = map(int, input().split())
+D[startDosi] = 0
+q.put((0, startDosi))
+
 
 while q.qsize() > 0:
     now = (q.get())[1]
-    if visited[now]: continue
+    if visited[now]:
+        continue
     visited[now] = True
-
-    for tmp in myList[now]:
+    for tmp in bus[now]:
         next = tmp[0]
         value = tmp[1]
-        if distance[next] > distance[now] + value:
-            distance[next] = distance[now] + value
-            q.put((distance[next], next))
-        
+        if D[next] > D[now] + value:
+            D[next] = D[now] + value
+            q.put((D[next], next))
 
-for i in range(1,V+1):
-    if visited[i]:
-        print(distance[i])
-    else:
-        print("INF")
+
+print(D[endDosi])

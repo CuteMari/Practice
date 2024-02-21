c9503d1807a8bdbef3ff415c930e12285eaf2da3
yumin <pikahchoo200@gmail.com>
2024-02-22 01:48:23 +0900

    [백준]11000

--- practice.py
+++ practice.py
@@ -1,45 +1,39 @@
 import sys
 input = sys.stdin.readline
+from queue import PriorityQueue
+import heapq
 
-def solution(s):
+def solution(N):
     answer = 0
-
-    min = len(s)
-    for l in range(1, len(s)+1):
-        length = len(s)
-        start = 0
-        end = l-1    
-        while start <= len(s) - 1:
-            if end > len(s) - 1:
-                tmp = s[start:len(s)-1]
-                break
-            else:
-                tmp = s[start:end+1]
-                cnt = 0
-                while s[start:end+1] == tmp:
-                    cnt += 1
-                    start += l
-                    end += l
-                if cnt > 1:
-                    length -= (cnt-1)*(len(tmp))
-                    length += len(str(cnt))
-
-
+       
+    pq = PriorityQueue()
+    for i in range(N):
+        S,T = map(int, input().split())
+        pq.put((S,T))
         
-        if length < min:
-            min = length
-            
-    answer = min
+    
+    room = [0]
+    while pq.qsize() > 0:
+        lecture = pq.get()
+        lecture_start = lecture[0]
+        lecture_end = lecture[1]
 
+        if lecture_start >= room[0]:
+            heapq.heappop(room)
+            heapq.heappush(room, lecture_end)
+        else:
+            heapq.heappush(room, lecture_end)
+
+    answer = len(room)
+   
+  
     return answer
 
 
 
 
 if __name__ == "__main__":
-    s = input().rstrip()
-    answer = solution(s)
-    print(answer)
-
-
+    N = int(input())
+    res = solution(N)
+    print(res)
 

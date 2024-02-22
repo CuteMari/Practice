6c2738c70e5d18b8b6192488c90ad0eb0bd94735
yumin <pikahchoo200@gmail.com>
2024-02-22 13:50:07 +0900

    [프로그래머스]3 x n 타일링

--- practice.py
+++ practice.py
@@ -1,32 +1,26 @@
 import sys
 input = sys.stdin.readline
-from queue import PriorityQueue
-import heapq
+
 
 def solution(N):
     answer = 0
-       
-    pq = PriorityQueue()
-    for i in range(N):
-        S,T = map(int, input().split())
-        pq.put((S,T))
-        
-    
-    room = [0]
-    while pq.qsize() > 0:
-        lecture = pq.get()
-        lecture_start = lecture[0]
-        lecture_end = lecture[1]
 
-        if lecture_start >= room[0]:
-            heapq.heappop(room)
-            heapq.heappush(room, lecture_end)
-        else:
-            heapq.heappush(room, lecture_end)
+    dp = [0 for _ in range(5001)]
+    dp[2] = 3
 
-    answer = len(room)
-   
-  
+    for i in range(3,5001):
+        if i%2 == 1:
+            continue
+        for j in range(2, i-2+1, 2):
+            if j == i-2: dp[i] += dp[2]*dp[j]
+            else: dp[i] += 2*dp[j]
+        dp[i] += 2
+
+        dp[i] %= 1000000007
+
+    answer = dp[N]
+
+
     return answer
 
 
@@ -36,4 +30,3 @@ if __name__ == "__main__":
     N = int(input())
     res = solution(N)
     print(res)
-

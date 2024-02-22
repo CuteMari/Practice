59523601520fa9db8639b13fc24ae0d734f615f7
yumin <pikahchoo200@gmail.com>
2024-02-22 15:03:42 +0900

    [백준]외판원 순회 2

--- practice.py
+++ practice.py
@@ -1,32 +1,26 @@
 import sys
 input = sys.stdin.readline
 
-
-def solution(N):
-    answer = 0
-
-    dp = [0 for _ in range(5001)]
-    dp[2] = 3
-
-    for i in range(3,5001):
-        if i%2 == 1:
-            continue
-        for j in range(2, i-2+1, 2):
-            if j == i-2: dp[i] += dp[2]*dp[j]
-            else: dp[i] += 2*dp[j]
-        dp[i] += 2
-
-        dp[i] %= 1000000007
-
-    answer = dp[N]
-
-
-    return answer
-
+def solution(depth, start, cost, graph, visited):
+    global M
+    if depth == len(visited)-1 and graph[start][0] != 0:
+        M = min(M, cost+graph[start][0])
+        return
+    for i in range(N):
+        if not visited[i] and graph[start][i] != 0:
+            visited[i] = True
+            solution(depth+1, i, cost+graph[start][i], graph, visited)
+            visited[i] = False
 
 
 
 if __name__ == "__main__":
     N = int(input())
-    res = solution(N)
-    print(res)
+    graph = [list(map(int, input().split())) for _ in range(N)]
+    visited = [False]*(N)
+    visited[0] = True
+    M = sys.maxsize
+
+    solution(0,0,0,graph,visited)
+
+    print(M)

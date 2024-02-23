56dd226b3e71482287b62fff73fca6497d80acd4
yumin <pikahchoo200@gmail.com>
2024-02-23 19:44:42 +0900

    [백준]N과 M (3)

--- practice.py
+++ practice.py
@@ -1,25 +1,26 @@
 import sys
 input = sys.stdin.readline
 
+
 def dfs(v, depth):
-    visited[v] = True
     ans.append(v)
     if depth == M:
         for i in range(len(ans)):
             print(ans[i], end=' ')
         print()
         return
-    for i in range(v+1, N+1):
-        if not visited[i]:
-            visited[i] = True
-            dfs(i, depth+1)
-            visited[i] = False
-            ans.pop()
-
-
+    for i in range(1, N+1):
+        visited[i] = True
+        dfs(i, depth+1)
+        visited[i] = False
+        ans.pop()
 
 N, M = map(int, input().split())
+
+
 for i in range(1,N+1):
-    visited = [False for _ in range(N+1)]
     ans = []
-    dfs(i, 1)
+    visited = [False for _ in range(N+1)]
+    visited[i] = True
+    dfs(i,1)
+

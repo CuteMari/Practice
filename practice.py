0c50f4c03e4ea20fe1c0daad8a7cfef23726ad4d
yumin <pikahchoo200@gmail.com>
2024-02-26 01:38:42 +0900

    [백준]누울 자리를 찾아라

--- practice.py
+++ practice.py
@@ -1,25 +1,43 @@
 import sys
 input = sys.stdin.readline
+import re
 from collections import deque
 
-def DFS(depth):
-    if depth == M:
-        for i in range(M):
-            print(ans[i], end=' ')
-        print()
-        return
-    for i in range(N):
-        ans.append(A[i])
-        DFS(depth+1)
-        ans.pop()
+N = int(input())
+room = [[] for _ in range(N)]
+for i in range(N):
+    room[i] = list(input().rstrip())
     
 
-
-N, M = map(int, input().split())
-A = list(map(int, input().split()))
-A.sort()
-
+row_cnt = 0
 for i in range(N):
-    ans = []
-    ans.append(A[i])
-    DFS(1)
+    row = room[i]
+    cnt = 0
+    for j in range(N):
+        if row[j] == 'X':
+            cnt = 0
+        elif row[j] == '.' and cnt >= 2:
+            continue
+        elif row[j] == '.' and cnt == 1:
+            row_cnt += 1
+            cnt += 1
+        elif row[j] == '.':
+            cnt += 1
+
+col_cnt = 0
+for i in range(N):
+    cnt = 0
+    for j in range(N):
+        if room[j][i] == 'X':
+            cnt = 0
+        elif room[j][i] == '.' and cnt >= 2:
+            continue
+        elif room[j][i] == '.' and cnt == 1:
+            col_cnt += 1
+            cnt += 1
+        elif room[j][i] == '.':
+            cnt += 1
+        
+    
+            
+print(row_cnt, col_cnt)

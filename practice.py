8ad1a1f3865a4ebbf8b345e12a753f1c5ea51047
yumin <pikahchoo200@gmail.com>
2024-02-28 14:30:44 +0900

    [백준]11399 ATM

--- practice.py
+++ practice.py
@@ -1,42 +1,20 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
 
-def Importance(D_cnt, k):
-    for i in range(9, k, -1):
-        if D_cnt[i] > 0:    # 높은 중요도가 존재하면 False
-            return False
+def solution(P):
+    arr = [0 for _ in range(len(P))]
 
-    return True  # 높은 중요도가 없으면 True
+    for i in range(1,len(P)):
+        arr[i] = arr[i-1] + P[i]
 
 
-
-T = int(input())
-for _ in range(T):
-    N, M = map(int, input().split())
-    D = [0] + list(map(int, input().split()))  
-    D_cnt = [0] * 10     # 중요도 cnt
-
-    for i in range(1,N+1):
-        D_cnt[D[i]] += 1
-
-    queue  = deque()
-    for i in range(1,N+1):   # 초기 큐 1(A) ~ Z(26) 저장
-        queue.append(i)
-
-    res = 0
-    target = M+1
-    while queue:  # 중요도 1 ~ 9
-        now = queue.popleft()
-        isPop = Importance(D_cnt, D[now])
-        if isPop:
-            res += 1
-            D_cnt[D[now]] -= 1
-            if now == target:
-                print(res)
-                break
-        else:
-            queue.append(now)
+    ans = sum(arr)
+    return ans
 
 
-
+if __name__ == '__main__':
+    N = int(input())
+    P = [0] + list(map(int, input().split()))   # 1번부터 시작!
+    P.sort()
+    res = solution(P)
+    print(res)

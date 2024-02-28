2beb9b3611ceb15cc3eb7bb885d7b36598b8fc98
yumin <pikahchoo200@gmail.com>
2024-02-28 16:53:09 +0900

    [프로그래머스]크레인 인형뽑기 게임

--- practice.py
+++ practice.py
@@ -1,33 +1,35 @@
 import sys
 input = sys.stdin.readline
 
-def solution(s):
-    alp = [0 for _ in range(26)]
-
-    isGroup = True
-    checkchar = ''
-    for i in s:
-        if i == checkchar:
-            continue
-        else:
-            if alp[ord(i)-97] == 1:
-                isGroup = False
+def solution(board, moves):
+    answer = 0
+    B = board
+    
+    stack = []
+    for col in moves:
+        for i in range(0, len(B)):
+            doll_num = B[i][col-1]
+            if doll_num > 0:  # 인형이 존재하면
+                B[i][col-1] = 0
+                if len(stack) == 0:
+                    stack.append(doll_num)
+                else:   # 스택에 인형이 존재하면
+                    if stack[-1] == doll_num:
+                        stack.pop()
+                        answer += 2
+                    else:
+                        stack.append(doll_num)
                 break
-            else:
-                alp[ord(i)-97] = 1
-                checkchar = i
 
 
-    return isGroup
-
+    return answer
 
 if __name__ == '__main__':
-    N = int(input())
-    cnt = 0
-    for _ in range(N):
-        s = input().rstrip()
-        res = solution(s)
-        if res:
-            cnt += 1
-
-    print(cnt)
+    board = [[0,0,0,0,0],  # 2차원 배열
+             [0,0,1,0,3],
+             [0,2,5,0,1],
+             [4,2,4,4,2],
+             [3,5,1,3,1]]
+    moves = [1,5,3,5,1,2,1,4]  # col단위
+    res = solution(board, moves)
+    print(res)

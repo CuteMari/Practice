f63a76098240f95260775009651e096ecde87bdd
yumin <pikahchoo200@gmail.com>
2024-03-01 02:43:52 +0900

    [백준]2579 계단 오르기

--- practice.py
+++ practice.py
@@ -1,35 +1,32 @@
 import sys
 input = sys.stdin.readline
 
-def solution(board, moves):
-    answer = 0
-    B = board
+
+def solution(N):
+        
+    DP = [0 for _ in range(N)]
+    DP[0] = A[0]
+    DP[1] = A[0] + A[1]
     
-    stack = []
-    for col in moves:
-        for i in range(0, len(B)):
-            doll_num = B[i][col-1]
-            if doll_num > 0:  # 인형이 존재하면
-                B[i][col-1] = 0
-                if len(stack) == 0:
-                    stack.append(doll_num)
-                else:   # 스택에 인형이 존재하면
-                    if stack[-1] == doll_num:
-                        stack.pop()
-                        answer += 2
-                    else:
-                        stack.append(doll_num)
-                break
+    for i in range(2, N):
+        DP[i] = max(A[i] + A[i-1] + DP[i-3], A[i] + DP[i-2])
+
+    
+    return DP[N-1]
 
 
-    return answer
 
 if __name__ == '__main__':
-    board = [[0,0,0,0,0],  # 2차원 배열
-             [0,0,1,0,3],
-             [0,2,5,0,1],
-             [4,2,4,4,2],
-             [3,5,1,3,1]]
-    moves = [1,5,3,5,1,2,1,4]  # col단위
-    res = solution(board, moves)
+    N = int(input())
+    A = []
+    for i in range(N):
+        A.append(int(input()))
+    
+    if N == 1:
+        res = A[0]
+    elif N == 2:
+        res = A[0] + A[1]
+    else:
+        res = solution(N)
+
     print(res)

b10d1b88bbbad58b25efbd5ff6763851f578a0b8
yumin <pikahchoo200@gmail.com>
2024-03-07 13:19:14 +0900

    [백준]1058 친구

--- practice.py
+++ practice.py
@@ -2,42 +2,34 @@ import sys
 input = sys.stdin.readline
 
 
-def backtrack(r,n,is_queen_in_col, is_queen_in_left, is_queen_in_right):
-    if r == n:
-        return 1
+N = int(input())
+A = [[] for _ in range(N+1)]
+for i in range(1,N+1):
+    s = input().rstrip()
+    for j in range(len(s)):
+        if s[j] == 'Y':
+            A[i].append(j+1)
+
+
+M = 0
+for i in range(1,N+1):
+    visited = [False]*(N+1)
+    visited[i] = True
+    for j in range(len(A[i])):
+        visited[A[i][j]] = True
+    
+    for j in range(len(A[i])):  # 친구의 친구 연결하기
+        sec_idx = A[i][j]
+        for k in A[sec_idx]:
+            if not visited[k]:
+                visited[k] = True
+             
+    cnt = visited.count(True)-1
+    M = max(M, cnt)
+print(M)
     
 
-    ret = 0
-
-    for c in range(n):
-        if not is_queen_in_col[c] and not is_queen_in_right[r+c] and not is_queen_in_left[n-1+r-c]:
-            is_queen_in_col[c] = True
-            is_queen_in_right[r+c] = True
-            is_queen_in_left[n-1+r-c] = True
 
 
-            ret += backtrack(r+1, n, is_queen_in_col, is_queen_in_left, is_queen_in_right)
+      
 
-
-            is_queen_in_col[c] = False
-            is_queen_in_right[r+c] = False
-            is_queen_in_left[n-1+r-c] = False
-
-
-    return ret
-
-
-def solution(n):
-    SIZE = 15
-    is_queen_in_col = [False]*((SIZE-1)*2)
-    is_queen_in_left = [False]*((SIZE-1)*2)
-    is_queen_in_right = [False]*((SIZE-1)*2)
-    
-    answer = backtrack(0,n,is_queen_in_col, is_queen_in_left, is_queen_in_right)
-
-    return answer
-
-if __name__ == "__main__":
-    N = int(input())
-    ans = solution(N)
-    print(ans)

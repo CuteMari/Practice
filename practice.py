06ede790dab69fc0269304113467a14aa03c33cb
yumin <pikahchoo200@gmail.com>
2024-03-07 14:22:18 +0900

    [백준]2178 미로 탐색

--- practice.py
+++ practice.py
@@ -1,35 +1,36 @@
 import sys
 input = sys.stdin.readline
+from collections import deque
 
-
-N = int(input())
-A = [[] for _ in range(N+1)]
-for i in range(1,N+1):
-    s = input().rstrip()
-    for j in range(len(s)):
-        if s[j] == 'Y':
-            A[i].append(j+1)
-
-
-M = 0
-for i in range(1,N+1):
-    visited = [False]*(N+1)
-    visited[i] = True
-    for j in range(len(A[i])):
-        visited[A[i][j]] = True
+def bfs(i,j):
+    queue = deque()
+    queue.append((i,j, 1))
     
-    for j in range(len(A[i])):  # 친구의 친구 연결하기
-        sec_idx = A[i][j]
-        for k in A[sec_idx]:
-            if not visited[k]:
-                visited[k] = True
-             
-    cnt = visited.count(True)-1
-    M = max(M, cnt)
-print(M)
-    
+    drow = [1,0,-1,0]
+    dcol = [0,1,0,-1]
+    ans = 0
+    while queue:
+        now = queue.popleft()
+        if now[0] == N-1 and now[1] == M-1:
+            ans = now[2]
+            break
+        for i in range(4):
+            row = now[0] + drow[i]
+            col = now[1] + dcol[i]
+            cnt = now[2]
+            if row >= 0 and row < N and col >= 0 and col < M and maze[row][col] == 1:
+                if not visited[row][col]:
+                    visited[row][col] = True
+                    queue.append((row, col, cnt+1))
+
+    print(ans)
+
+N, M = map(int, input().split())
+maze = []
+for _ in range(N):
+    maze.append(list(map(int, input().rstrip())))
 
 
+visited = [[False for _ in range(M)] for _ in range(N)]
 
-      
-
+bfs(0,0)

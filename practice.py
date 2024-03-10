443b0d65231093589c3bf6fde49ec0ecaefbd8e7
yumin <pikahchoo200@gmail.com>
2024-03-10 21:52:10 +0900

    [백준]13424 비밀 모임

--- practice.py
+++ practice.py
@@ -1,25 +1,43 @@
 import sys
 input = sys.stdin.readline
 
+
+def floydWarshall(A):
+    for k in range(1,N+1):
+        for i in range(1, N+1):
+            for j in range(1, N+1):
+                A[i][j] = min(A[i][j], A[i][k] + A[k][j])
+
+
+def calcDistance(roomNum, A, P):
+    distance = 0
+    for i in P:
+        distance += A[i][roomNum]
+
+    return distance
+
+
 T = int(input())
 for _ in range(T):
-    log = input().rstrip()
-    front = []
-    back = []
-    cursor = 0
+    N, M = map(int, input().split())
+    arr = [[sys.maxsize for _ in range(N+1)] for _ in range(N+1)]
+    for i in range(1, N+1):
+        arr[i][i] = 0
+    for _ in range(M):
+        a, b, c = map(int, input().split())
+        arr[a][b] = c
+        arr[b][a] = c
 
-    for c in log:
-        if c == '-':
-            if front:
-                front.pop()
-        elif c == '<':
-            if front:
-                back.append(front.pop())
-        elif c == '>':
-            if back:
-                front.append(back.pop())
-        else:
-            front.append(c)
+    floydWarshall(arr)
 
-       
-    print("".join(front) + "".join(back[::-1]))
+
+    K = int(input())
+    roomP = list(map(int, input().split()))
+    minValue = sys.maxsize 
+    minNum = 0
+    for i in range(1,N+1):
+        if minValue > calcDistance(i, arr, roomP):
+            minValue = calcDistance(i, arr, roomP)
+            minNum = i
+
+    print(minNum)

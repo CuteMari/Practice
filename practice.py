8222973b8755f3d9e2b9d5b6535d18c11cebe4d7
yumin <pikahchoo200@gmail.com>
2024-04-02 00:11:10 +0900

    [백준]7512 연속하는 소수의 합

--- practice.py
+++ practice.py
@@ -1,20 +1,49 @@
 import sys
 input = sys.stdin.readline
+import math
+
+Prime = [True]*(10000000)
+for i in range(2, int(math.sqrt(10000000))+1):
+    if Prime[i]:
+        for j in range(i*2, 10000000, i):
+            Prime[j] = False
 
 
-N = int(input())
-A = list(map(int, input().split()))
-K = int(input())
+primeList = []
+for i in range(2,10000000):
+    if Prime[i]:
+        primeList.append(i)
 
-ans = [[] for _ in range(K)]
-idx = 0
-for i in range(0, N, N//K):
-    tmp = A[i:i+N//K]
-    tmp.sort()
-    ans[idx] = tmp
-    idx += 1
 
-for k in ans:
-    for j in k:
-        print(j, end=' ')
+T = int(input())
+for t in range(T):
+    M = int(input())
+    A = list(map(int, input().split()))
+    checkdic = {}
+    for l in A:
+        tmp = 0
+        for i in range(l):
+            tmp += primeList[i]
+        checkdic[tmp] = 1
+        for i in range(l, len(primeList)):
+            j = i - l
+            tmp -= primeList[j]
+            tmp += primeList[i]
+            if tmp >= 10000000: break
+            
+            if tmp in checkdic:
+                checkdic[tmp] += 1
+            else:
+                checkdic[tmp] = 1
+        
+    
+    print("Scenario " + str(t+1) + ":")
+    
+    for i in checkdic.keys():
+        if Prime[i] and checkdic[i] == M:
+            print(i)
+            break
+    
+    print()
+
 

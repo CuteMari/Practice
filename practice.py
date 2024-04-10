6dbfe4f0cab5e3abebf98ade67240d365968dac7
yumin <pikahchoo200@gmail.com>
2024-04-10 20:40:31 +0900

    [백준]2346 풍선 터뜨리기

--- practice.py
+++ practice.py
@@ -1,22 +1,32 @@
 import sys
 input = sys.stdin.readline
+from collections import deque
 
+N = int(input())
+A = list(map(int, input().split()))
 
+total = 0
+idx = 0
+while True:
+    if total == N-1:
+        print(idx+1)
+        break
+    print(idx+1, end=' ')
+    cnt = A[idx]
+    A[idx] = 0
+    if cnt < 0:
+        tmp = abs(cnt)
+        while tmp > 0:
+            idx = (idx - 1 + N)%N
+            if A[idx] != 0:
+                tmp -= 1
+    elif cnt > 0:
+        tmp = 0
+        while tmp < cnt:
+            idx = (idx + 1 + N)%N
+            if A[idx] != 0:
+                tmp += 1
+    total += 1
 
-cnt = 0
-N, P = map(int, input().split())
-finger = [[] for _ in range(N+1)]
-for _ in range(N):
-    a, b = map(int, input().split())
-    while len(finger[a]) > 0 and finger[a][-1] > b:
-        finger[a].pop()
-        cnt += 1
-    if len(finger[a]) > 0 and finger[a][-1] == b:
-        continue
-    finger[a].append(b)
-    cnt += 1
-
-    
-print(cnt)
-        
-
+            
+            

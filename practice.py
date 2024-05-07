c53144dc620fdf90410382eebfdb051defdeeb6c
yumin <pikahchoo200@gmail.com>
2024-05-07 23:29:32 +0900

    [백준]10989 수 정렬하기 3

--- practice.py
+++ practice.py
@@ -3,30 +3,26 @@ input = sys.stdin.readline
 from collections import deque
 
 N = int(input())
-A = list(map(int, input().split()))
+A = []
+for _ in range(N):
+    A.append(int(input()))
 
-total = 0
-idx = 0
-while True:
-    if total == N-1:
-        print(idx+1)
-        break
-    print(idx+1, end=' ')
-    cnt = A[idx]
-    A[idx] = 0
-    if cnt < 0:
-        tmp = abs(cnt)
-        while tmp > 0:
-            idx = (idx - 1 + N)%N
-            if A[idx] != 0:
-                tmp -= 1
-    elif cnt > 0:
-        tmp = 0
-        while tmp < cnt:
-            idx = (idx + 1 + N)%N
-            if A[idx] != 0:
-                tmp += 1
-    total += 1
+buckets = [deque() for _ in range(10)]
 
-            
-            
+max_val = max(A)
+queue = deque(A)
+digit = 1
+
+while max_val >= digit:
+    while queue:
+        num = queue.popleft()
+        buckets[(num // digit) % 10].append(num)
+    
+    for bucket in buckets:
+        while bucket:
+            queue.append(bucket.popleft())
+    digit *= 10
+
+
+while queue:
+    print(queue.popleft())
